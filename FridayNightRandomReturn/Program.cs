da0c396074c4647beb119f99c6a38b8a3722e449
Jaak Valter Lõhmuspuu <jlohmusp@tktk.ee>
2021-12-08 23:21:23 +0200

    Functions, Arrays, FromFile

---
+++ FridayNightRandomReturn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FridayNightRandomReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomFood, randomDrink, randomMovie;
+            string[] foods = { "chicken", "pizza", "sushi", "popcorn", "salad" };
+            string[] drinks = { "Sprite", "Coca-Cola", "Fanta", "orange juice", "water" };
+            string[] movies = { "Die Hard", "Terminator", "Grinch", "Home Alone", "Harry Potter" };
+
+            randomFood = PickRandomElementFromArray(foods);
+            randomDrink = PickRandomElementFromArray(drinks);
+            randomMovie = PickRandomElementFromArray(movies);
+
+            Console.WriteLine($"Tonight you will have some {randomFood} with {randomDrink} and watch {randomMovie}.");
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string PickRandomElementFromArray(string[] someArray)
+        {
+            string randomElement = someArray[GenerateRandomIndex(someArray)];
+            return randomElement;
+        }
+
+    }
+}
